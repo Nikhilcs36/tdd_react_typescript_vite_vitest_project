2824d8ed1c57fe08cde1f35a956d28eb9781c2f5
Nikhil <nikhilcs36@gmail.com>
2024-12-26 00:24:57 +0530

    fixed TypeScript doesn't recognize the test property in the Vite configuration ( solution https://stackoverflow.com/a/79028899)

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
@@ -8,4 +8,6 @@ export default defineConfig({
       environment: 'jsdom',
       setupFiles: "./src/tests/setup.ts"
     },
-})
+}as UserConfig)
+
+// fixed TypeScript doesn't recognize the test property in the Vite configuration ( solution https://stackoverflow.com/a/79028899)  
