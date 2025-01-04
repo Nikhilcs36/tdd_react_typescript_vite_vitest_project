e7a27b78ca559b001e473dd5b67c25b78ffa56e9
Nikhil <nikhilcs36@gmail.com>
2025-01-05 03:04:06 +0530

    add test 'sends username, email and password to backend after submit a button'in SignUpPage.spec.tsx and implement submit logic in SignUpPage.tsx

--- src/page/SignUpPage.tsx
+++ src/page/SignUpPage.tsx
@@ -1,14 +1,27 @@
-import { Component} from "react";
+import axios from "axios";
+import { Component } from "react";
 
 interface SignUpState {
+  username: string;
+  email: string;
   password: string;
   passwordRepeat: string;
 }
 
 class SignUpPage extends Component<{}, SignUpState> {
   state: SignUpState = {
+    username: "",
+    email: "",
     password: "",
     passwordRepeat: "",
+  };
+
+  onChangeUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ username: event.target.value });
+  };
+
+  onChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ email: event.target.value });
   };
 
   onChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -24,19 +37,36 @@ class SignUpPage extends Component<{}, SignUpState> {
     return !(password && passwordRepeat && password === passwordRepeat);
   };
 
+  submit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const { username, email, password, passwordRepeat } = this.state;
+    const body = { username, email, password, passwordRepeat };
+    axios.post("/api/1.0/users", body);
+  };
+
   render() {
     return (
       <div>
-        <h1>Sign Up</h1>
-        <label htmlFor="username">Username</label>
-        <input id="username" />
-        <label htmlFor="email">E-mail</label>
-        <input id="email" />
-        <label htmlFor="password">Password</label>
-        <input id="password" type="password" onChange={this.onChangePassword} />
-        <label htmlFor="passwordRepeat">Password Repeat</label>
-        <input id="passwordRepeat" type="password" onChange={this.onChangePasswordRepeat} />
-        <button disabled={this.isDisabled()}>Sign Up</button>
+        <form onSubmit={this.submit}>
+          <h1>Sign Up</h1>
+          <label htmlFor="username">Username</label>
+          <input id="username" onChange={this.onChangeUsername} />
+          <label htmlFor="email">E-mail</label>
+          <input id="email" onChange={this.onChangeEmail} />
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            onChange={this.onChangePassword}
+          />
+          <label htmlFor="passwordRepeat">Password Repeat</label>
+          <input
+            id="passwordRepeat"
+            type="password"
+            onChange={this.onChangePasswordRepeat}
+          />
+          <button disabled={this.isDisabled()}>Sign Up</button>
+        </form>
       </div>
     );
   }
