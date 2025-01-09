d4b67b28f455beab04ab0856aadc1c9943711c0f
Nikhilcs36 <nikhilcs36@gmail.com>
2025-01-10 00:17:03 +0530

    add and configure dependency twin.macro for styling with Tailwind css with CSS-in-JS and also update signup page // reference: https://github.com/ben-rogerson/twin.examples/tree/master/vite-styled-components-typescript

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,19 @@ import { defineConfig, UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [react()],
+    optimizeDeps: {
+      esbuildOptions: {
+        target: 'es2020',
+      },
+    },
+    esbuild: {
+      // https://github.com/vitejs/vite/issues/8644#issuecomment-1159308803
+      logOverride: { 'this-is-undefined-in-esm': 'silent' },
+    },
+    plugins: [react({
+      babel: {
+        plugins: ['babel-plugin-macros', 'babel-plugin-styled-components'],
+      },})],
     test: {
       globals: true,
       environment: 'jsdom',
