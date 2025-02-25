0f42f36e37743b1f44d626eccdd3a546e1095416
Nikhilcs36 <nikhilcs36@gmail.com>
2025-02-25 18:21:22 +0530

    add dependency 'react-router-dom ==7.2.0' and refactor the app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { I18nextProvider, useTranslation } from "react-i18next";
 import i18n from "./locale/i18n";
 import LanguageSwitcher from "./locale/languageSwitcher";
@@ -6,7 +7,6 @@ import HomePage from "./page/HomePage";
 import LoginPage from "./page/LoginPage";
 import UserPage from "./page/UserPage";
 import { axiosApiService } from "./services/apiService";
-import { useState } from "react";
 import tw from "twin.macro";
 
 // Navbar styled components
@@ -29,7 +29,7 @@ const NavLeft = tw.div`flex items-center`;
 
 const NavRight = tw.div`flex items-center gap-6`;
 
-const NavLink = tw.a`
+const NavLink = tw(Link)`
   font-semibold 
   cursor-pointer 
   hover:underline 
@@ -44,41 +44,32 @@ const Content = tw.div`
 
 function App() {
   const { t } = useTranslation();
-  const [path, setPath] = useState(window.location.pathname);
-
-  const onclickLink = (event: React.MouseEvent<HTMLAnchorElement>) => {
-    event.preventDefault();
-    const newPath = event.currentTarget.getAttribute("href");
-    if (newPath) {
-      window.history.pushState({}, "", newPath);
-      setPath(newPath);
-    }
-  };
-
   return (
     <I18nextProvider i18n={i18n}>
-      <NavBar>
-        <NavLeft>
-          <NavLink href="/" title="Home" onClick={onclickLink}>
-            Home
-          </NavLink>
-        </NavLeft>
-        <NavRight>
-          <NavLink href="/signup" onClick={onclickLink}>
-            {t("signup.title")}
-          </NavLink>
-          <NavLink href="/login" onClick={onclickLink}>
-            Login
-          </NavLink>
-        </NavRight>
-      </NavBar>
-
-      <Content>
-        {path === "/" && <HomePage />}
-        {path === "/signup" && <SignUpPage apiService={axiosApiService} />}
-        {path === "/login" && <LoginPage />}
-        {path.startsWith("/user/") && <UserPage />}
-      </Content>
+      <Router>
+        <NavBar>
+          <NavLeft>
+            <NavLink to="/" title="Home">
+              Home
+            </NavLink>
+          </NavLeft>
+          <NavRight>
+            <NavLink to="/signup">{t("signup.title")}</NavLink>
+            <NavLink to="/login">Login</NavLink>
+          </NavRight>
+        </NavBar>
+        <Content>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route
+              path="/signup"
+              element={<SignUpPage apiService={axiosApiService} />}
+            />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/user/:id" element={<UserPage />} />
+          </Routes>
+        </Content>
+      </Router>
       <LanguageSwitcher />
     </I18nextProvider>
   );
