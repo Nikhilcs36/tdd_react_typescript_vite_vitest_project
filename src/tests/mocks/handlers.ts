62830419c6ede04328556c76306fa20fc704173e
Nikhilcs36 <124907759+Nikhilcs36@users.noreply.github.com>
2025-03-05 03:03:02 +0530

    add test 'Account Activation Page' in accountActivationPage.test.tsx and implement AccountActivationPage logic in accountActivationPage.tsx and update mocks/handlers.ts with Mock API for account activation response and update url AccountActivationPage with token parameters in app.tsx (#78)
    Signed-off-by: Nikhilcs36 <nikhilcs36@gmail.com>

--- src/tests/mocks/handlers.ts
+++ src/tests/mocks/handlers.ts
@@ -2,6 +2,7 @@ import { http, HttpResponse } from "msw";
 import { SignUpRequestBody, validateSignUp } from "../../utils/validationRules";
 
 export const handlers = [
+  // Mock API for user signup
   http.post("/api/1.0/users", async ({ request }) => {
     // Capture the Accept-Language header from the request.
     const acceptLanguage = request.headers.get("Accept-Language");
@@ -24,11 +25,26 @@ export const handlers = [
         { status: 400 }
       );
     }
-
     // If no validation errors, simulate a successful response
     return HttpResponse.json(
       { message: "User created", languageReceived: acceptLanguage },
       { status: 200 }
     );
+  }),
+
+  // Mock API for account activation
+  http.post("/api/1.0/users/token/:token", async ({ params }) => {
+    // Extract the token from request parameters
+    const { token } = params;
+
+    // Simulate activation success if the token is not 'invalid'
+    if (token === "invalid") {
+      return HttpResponse.json(
+        { message: "Activation Failed" },
+        { status: 400 }
+      );
+    }
+
+    return HttpResponse.json({ message: "Account Activated" }, { status: 200 });
   }),
 ];
