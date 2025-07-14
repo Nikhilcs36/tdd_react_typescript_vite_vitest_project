643921551a2abd1b1c72c4a7ace951da8dfebee9
Nikhilcs36 <nikhilcs36@gmail.com>
2025-07-14 20:15:05 +0530

    set up theme and language direction combinations logic and test

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import tw from "twin.macro";
-
+import { useTranslation } from "react-i18next";
 // Define styled components
 const SwitcherContainer = tw.div`
   w-12
@@ -56,6 +56,22 @@ export const ThemeSwitcher: React.FC<ThemeSwitcherProps> = ({
   onClick,
   theme,
 }) => {
+  const { i18n } = useTranslation();
+  const dir = i18n.dir();
+
+  const isRTL = dir === "rtl";
+
+  const getTransform = () => {
+    if (theme === "light") {
+      return "translateX(0)";
+    }
+    if (isRTL) {
+      return "translateX(calc(-100% - 4px))";
+    } else {
+      return "translateX(calc(100% + 4px))";
+    }
+  };
+
   return (
     <SwitcherContainer
       onClick={onClick}
@@ -64,39 +80,38 @@ export const ThemeSwitcher: React.FC<ThemeSwitcherProps> = ({
     >
       <IconContainer
         style={{
-          transform:
-            theme === "light" ? "translateX(0)" : "translateX(calc(100% + 4px))",
+          transform: getTransform(),
         }}
       >
         {theme === "light" ? (
           <SunIcon
             xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M12 3v1m0 16v1m8.66-12.66l-.7.7M4.04 19.96l-.7.7M21 12h-1M4 12H3m16.66 7.34l-.7-.7M4.04 4.04l-.7-.7"
-              />
-            </SunIcon>
-          ) : (
-            <MoonIcon
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-              />
-            </MoonIcon>
-          )}
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M12 3v1m0 16v1m8.66-12.66l-.7.7M4.04 19.96l-.7.7M21 12h-1M4 12H3m16.66 7.34l-.7-.7M4.04 4.04l-.7-.7"
+            />
+          </SunIcon>
+        ) : (
+          <MoonIcon
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+            />
+          </MoonIcon>
+        )}
       </IconContainer>
     </SwitcherContainer>
   );
